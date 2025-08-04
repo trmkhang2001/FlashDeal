3afd077324f637f550c3ccc66452752b64d1945e
Minh Khang <trmkhang2001@gmail.com>
2025-08-04 15:47:09 +0700

    up favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="icon" type="image/png" href="{{ asset('/flashdeal/logo.png') }}" />
     {{-- <link href="https://flashdeales.com/css/materialdesignicons.min.css?v=1.1.5" rel="stylesheet" type="text/css"
         media="all" />
     <link href="https://flashdeales.com/css/pe-icon-7-stroke.css?v=1.1.5" rel="stylesheet" type="text/css"
